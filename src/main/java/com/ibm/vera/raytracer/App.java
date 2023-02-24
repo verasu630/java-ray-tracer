85ce7b111cd6bf359f2c3d07497bfae1f53adee6
Vera Su <vera0630@gmail.com>
2023-02-24 15:09:29 +1100

    Initial commit

---
+++ src/main/java/com/ibm/vera/raytracer/App.java
@@ -0,0 +1,7 @@
+package com.ibm.vera.raytracer;
+
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
---
+++ src/test/java/raytracer/AppTest.java
@@ -0,0 +1,15 @@
+package raytracer;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class AppTest {
+
+	@Test
+	public void test() {
+		assertTrue(true);
+	}
+
+}
