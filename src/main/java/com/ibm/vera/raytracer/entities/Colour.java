e93c4f50fb8feb6097ef5e2d24479a1450060aa7
Vera Su <vera0630@gmail.com>
2023-03-03 14:33:35 +1100

    Add colour and colour operations

---
+++ src/main/java/com/ibm/vera/raytracer/entities/Colour.java
@@ -0,0 +1,18 @@
+package com.ibm.vera.raytracer.entities;
+
+public class Colour extends Tuple {
+
+	public Colour(double red, double green, double blue) {
+		super(red, green, blue, 0);
+	}
+	
+	public static Colour hadamard_product(Colour c1, Colour c2) {
+		Colour result = new Colour(
+				c1.getX() * c2.getX(),
+				c1.getY() * c2.getY(),
+				c1.getZ() * c2.getZ()
+				);
+		
+		return result;
+	}
+}
